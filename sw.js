1d82bb1c73c6656ae7a702411818a66539b0f1ed
studentloanpim <studenloanpimlineoa@gmail.com>
2026-08-07 10:18:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const PRECACHE_URLS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
